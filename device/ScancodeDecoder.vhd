19e0b7c9e73ba78394a88ce5ff3b1238e3d29a3c
alexh <alex.horstkoetter@t-online.de>
2022-11-03 03:49:31 +0100

    implemented scancode decoding

---
+++ device/ScancodeDecoder.vhd
@@ -0,0 +1,84 @@
+--! Alexander Horstkötter 02.11.2022
+--! https://www.avrfreaks.net/sites/default/files/PS2%20Keyboard.pdf
+--! receives raw scancodes as bytes from keyboard and emits 
+--! extended 16-bit keycodes + break flag
+--! successfully simulated 03.11.2022
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity ScancodeDecoder is
+    port (
+        clk, nrst     : in std_logic;
+        s_tready      : out std_logic;
+        s_tvalid      : in std_logic;
+        s_tdata       : in std_logic_vector(7 downto 0);
+        m_tready      : in std_logic;
+        m_tvalid      : out std_logic;
+        m_tdata_code  : out std_logic_vector(15 downto 0);
+        m_tdata_break : out std_logic);
+end entity;
+
+architecture behavioral of ScancodeDecoder is
+    type DecodeState is (
+        IDLE,           -- ready to receive new scancode
+        EXTENDED,       -- already has first half of extended keycode
+        BREAK,          -- already as break flag
+        EXTENDED_BREAK, -- both extended and break
+        TRANSMIT);      -- scancode complete. waiting for downstream handshake
+    signal decode_state : DecodeState;
+begin
+    s_tready <= '0' when decode_state = TRANSMIT or nrst = '0' else '1';
+    m_tvalid <= '1' when decode_state = TRANSMIT and nrst = '1' else '0';
+
+    process (clk) begin
+        if rising_edge(clk) then
+            if nrst = '0' then
+                decode_state  <= IDLE;
+                m_tdata_code  <= (others => '0');
+                m_tdata_break <= '0';
+            else
+                -- slave stream handshake
+                if s_tvalid and s_tready then
+                    case decode_state is
+                        when IDLE =>
+                            case s_tdata is
+                                when x"E0" =>
+                                    decode_state <= EXTENDED;
+                                    m_tdata_code <= x"00E0";
+                                when x"F0" =>
+                                    decode_state  <= BREAK;
+                                    m_tdata_break <= '1';
+                                when others =>
+                                    decode_state <= TRANSMIT;
+                                    m_tdata_code <= x"00" & s_tdata;
+                            end case;
+                        when EXTENDED =>
+                            case s_tdata is
+                                when x"F0" =>
+                                    decode_state  <= EXTENDED_BREAK;
+                                    m_tdata_break <= '1';
+                                when others =>
+                                    decode_state <= TRANSMIT;
+                                    m_tdata_code <= x"E0" & s_tdata;
+                            end case;
+                        when BREAK =>
+                            decode_state <= TRANSMIT;
+                            m_tdata_code <= x"00" & s_tdata;
+                        when EXTENDED_BREAK =>
+                            decode_state <= TRANSMIT;
+                            m_tdata_code <= x"E0" & s_tdata;
+                        when TRANSMIT =>
+                    end case;
+                end if;
+
+                -- master stream handshake
+                if decode_state = TRANSMIT and m_tready = '1' then
+                    decode_state  <= IDLE;
+                    m_tdata_break <= '0';
+                end if;
+            end if;
+        end if;
+    end process;
+end architecture;
---
+++ simulation/ScancodeDecoder_TB.vhd
@@ -0,0 +1,74 @@
+--! ScancodeDecoder_TB.vhd
+--! Alexander Horstkötter 03.11.2022
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity ScancodeDecoder_TB is
+end entity;
+
+architecture behavioral of ScancodeDecoder_TB is
+    component ScancodeDecoder
+        port (
+            clk, nrst     : in std_logic;
+            s_tready      : out std_logic;
+            s_tvalid      : in std_logic;
+            s_tdata       : in std_logic_vector(7 downto 0);
+            m_tready      : in std_logic;
+            m_tvalid      : out std_logic;
+            m_tdata_code  : out std_logic_vector(15 downto 0);
+            m_tdata_break : out std_logic);
+    end component;
+
+    type ScanCodes is array(0 to 7) of std_logic_vector(7 downto 0);
+    constant scan_codes : ScanCodes := (
+        x"1C",              -- make A
+        x"F0", x"1C",       -- break A
+        x"E0", x"70",       -- make INSERT
+        x"E0", x"F0", x"70" -- break INSERT
+    );
+
+    signal clk, nrst            : std_logic := '0';
+    signal in_tready, in_tvalid : std_logic;
+    signal in_tdata             : std_logic_vector(7 downto 0);
+    signal out_tready           : std_logic := '1';
+    signal out_tvalid           : std_logic;
+    signal out_tdata_break      : std_logic;
+    signal out_tdata_code       : std_logic_vector(15 downto 0);
+begin
+    DUT : ScancodeDecoder
+    port map(
+        clk           => clk,
+        nrst          => nrst,
+        s_tready      => in_tready,
+        s_tvalid      => in_tvalid,
+        s_tdata       => in_tdata,
+        m_tready      => out_tready,
+        m_tvalid      => out_tvalid,
+        m_tdata_break => out_tdata_break,
+        m_tdata_code  => out_tdata_code);
+
+    gen_clk : process begin
+        wait for 5 ps;
+        clk <= not clk;
+    end process;
+
+    gen_stim : process begin
+        wait for 20 ps;
+        nrst <= '1';
+
+        for i in scan_codes'range loop
+            if in_tready = '0' then
+                wait until in_tready = '1';
+            end if;
+
+            in_tvalid <= '1';
+            in_tdata  <= scan_codes(i);
+
+            wait for 10 ps;
+            in_tvalid <= '0';
+            wait for 10 ps;
+        end loop;
+    end process;
+end architecture;
